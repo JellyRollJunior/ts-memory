f8acc866e1828e3fd78866ad1c92b0b15cc5949a
Brandon Lin <brandonlin9716@gmail.com>
2025-12-16 16:59:27 +0800

    Center app content

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 function App() {
   return (
-    <>
+    <div className="flex min-h-screen min-w-screen flex-col items-center">
       <h1 className="text-3xl font-bold text-slate-700">Hello World!</h1>
-    </>
+    </div>
   );
 }
 
