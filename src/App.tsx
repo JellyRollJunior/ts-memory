73f3a7f4ce0b89363a3ef5f53567d388c06a9987
Brandon Lin <brandonlin9716@gmail.com>
2025-12-19 23:02:12 +0800

    Add footer decoration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { Header } from './components/Header.tsx';
 import { Gameboard } from './components/Gameboard.tsx';
+import { Footer } from './components/Footer.tsx';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <main className="flex w-full justify-center px-3 mt-1">
         <Gameboard />
       </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+  return (
+    <footer className="pt-12 md:pt-16">
+      <div className="h-9 w-full bg-[url(../assets/footer-bg.png)] bg-contain bg-center" />
+      <div className="bg-sand-beige h-36"></div>
+    </footer>
+  );
+};
+
+export { Footer };
