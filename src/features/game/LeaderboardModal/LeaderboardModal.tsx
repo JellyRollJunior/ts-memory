eee93901607fbd997ffec26da9af8a25b6d54d99
Brandon Lin <brandonlin9716@gmail.com>
2025-12-31 15:42:02 +0800

    Render list of winning names on LeaderboardModal

--- src/features/game/LeaderboardModal/LeaderboardModal.tsx
+++ src/features/game/LeaderboardModal/LeaderboardModal.tsx
@@ -1,15 +1,21 @@
 import { Modal } from '@/components/Modal';
+import SimpleBar from 'simplebar-react';
+import gold from '@/assets/svgs/trophy-gold.svg';
+import silver from '@/assets/svgs/trophy-silver.svg';
+import bronze from '@/assets/svgs/trophy-bronze.svg';
 
 type leaderboardModalProps = {
   className: string;
   isOpen: boolean;
   closeModal: () => void;
+  winners?: string[];
 };
 
 const LeaderboardModal = ({
   className = '',
   isOpen,
   closeModal,
+  winners = ['bob', 'billy', 'joe', 'usagi', 'luna', 'joe', 'usagi', 'luna', 'joe', 'usagi', 'luna'],
 }: leaderboardModalProps) => {
   return (
     <Modal
@@ -17,10 +23,29 @@ const LeaderboardModal = ({
       open={isOpen}
       showLogo={false}
       headerText="Leaderboard"
+      contentWrapperStyling="w-full"
     >
-      <div className="mt-2">This is the leaderboard!</div>
+      <SimpleBar className="border-sand-beige mt-3 h-42 border-t-2 border-b-2 px-6">
+        <ol className="my-2 flex flex-col gap-0.5">
+          {winners.map((winner, index) => (
+            <li className="grid w-full grid-cols-[24px_1fr_24px] items-center gap-2">
+              <span>{index + 1}.</span>
+              <span className="text-lg">{winner}</span>
+              {index == 0 ? (
+                <img src={gold} />
+              ) : index == 1 ? (
+                <img src={silver} />
+              ) : index == 2 ? (
+                <img src={bronze} />
+              ) : (
+                <div />
+              )}
+            </li>
+          ))}
+        </ol>
+      </SimpleBar>
       <button
-        className="secondary-button mx-auto mt-2 block px-5 py-0.5"
+        className="secondary-button mx-auto mt-4 block px-5 py-0.5"
         onClick={closeModal}
       >
         Close
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { routes } from './routes.tsx';
+import 'simplebar-react/dist/simplebar.min.css';
 
 const router = createBrowserRouter(routes);
 
